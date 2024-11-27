73356b4b9113bbd2c43c18b2c26108a01b330fe8
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-27 13:35:58 +0600

    coffee data post and get added

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req,res)=>{
-    res.send(`server running on port ${port}`);
+app.get('/', (req, res) => {
+  res.send(`server running on port ${port}`);
 });
 
 
@@ -32,16 +32,32 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-   
-    await client.db("admin").command({ ping: 1 });
+
+    // await client.db("admin").command({ ping: 1 });
+    const coffeeCollection = client.db('insertDB').collection('coffees')
+    app.get('/coffee', async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+    })
+
+
+
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
-app.listen(port , ()=>{
-    console.log(`local Server in runnign on port ${port}`)
+app.listen(port, () => {
+  console.log(`local Server in runnign on port ${port}`)
 })
